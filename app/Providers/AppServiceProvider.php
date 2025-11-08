668fbb879b0e5b1f1f00e6763a64cba67008ecc2
irmnrhs <irmnrmhs@gmail.com>
2025-11-08 09:38:34 +0700

    multirole access

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,30 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $roles = [
+            'Super Admin',
+            'Admin Bahan Baku',
+            'Admin Produksi',
+            'Admin Kontrol Kualitas',
+            'Admin Keamanan Pangan',
+            'Admin Ekspor',
+            'User'
+        ];
+
+        foreach ($roles as $role) {
+            // Gate::define($role, function ($user) use ($role) {
+            //     return $user->role->name === $role;
+            // });
+
+            Gate::define($role, function ($user) use ($role) {
+                if ($user->role->name === 'Super Admin') return true;
+                return $user->role->name === $role;
+            });
+
+        }
+
+        Gate::define('view-dashboard', function ($user) {
+            return $user->role->name !== 'User';
+        });
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,16 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            RoleSeeder::class,
+            UserSeeder::class,
+        ]);
+        
         // User::factory(10)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-
-        $this->call([
-            RoleSeeder::class,
-            UserSeeder::class,
-        ]);
     }
 }
