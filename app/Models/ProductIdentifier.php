175f0966c7572e36d81b80b1d01967804abccf01
irmnrmhs <irmnrmhs@gmail.com>
2025-12-27 09:19:14 +0700

    perbaikan identifier (kode tidak terdapat supplier)

--- app/Models/ProductIdentifier.php
+++ app/Models/ProductIdentifier.php
@@ -40,32 +40,3 @@ class ProductIdentifier extends Model
     //     return $this->berat - $this->berat;
     // }
 }
-
-// before
-// <?php
-
-// namespace App\Models;
-
-// use Illuminate\Database\Eloquent\Model;
-
-// class ProductIdentifier extends Model
-// {
-//     protected $fillable = [
-//         'rms_id',
-//         'grades_id',
-//         'kode',
-//         'tanggal',
-//         'biji',
-//         'berat'
-//     ];
-
-//     public function rawMaterial()
-//     {
-//         return $this->belongsTo(RawMaterial::class, 'rms_id');
-//     }
-
-//     public function grade()
-//     {
-//         return $this->belongsTo(Grade::class, 'grades_id');
-//     }
-// }
