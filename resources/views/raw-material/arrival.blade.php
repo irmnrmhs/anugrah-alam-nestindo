8b8bd896a05f13a92c6680532e47bc257dcb9ceb
irmnrmhs <irmnrmhs@gmail.com>
2025-12-29 15:51:12 +0700

    perbaikan

--- resources/views/raw-material/arrival.blade.php
+++ resources/views/raw-material/arrival.blade.php
@@ -129,7 +129,7 @@
             Swal.fire('Gagal', res.message || 'Terjadi kesalahan!', 'error');
         }
     })
-    .catch(() => Swal.fire('Error', 'Gagal menambahkan data. Pastikan kondisi diisi dan SKP belum digunakan.', 'error'));
+    .catch(() => Swal.fire('Error', 'Gagal menambahkan data. Pastikan kondisi diisi dan kode tidak duplikat.', 'error'));
 @stop
 
 @section('custom-js')
