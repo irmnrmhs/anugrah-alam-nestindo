719d26f790452c6afd0e45e6bf10a3c5b99bdc4a
irmnrmhs <irmnrmhs@gmail.com>
2026-02-21 07:47:27 +0700

    fix production modul layout

--- resources/views/production/wash.blade.php
+++ resources/views/production/wash.blade.php
@@ -46,12 +46,12 @@
             @endforeach
         </select>
     </div>
-    <div class="row mt-3">
-        <div class="col-md-4">
+    <div class="row mt-3 justify-content-center">
+        <div class="col-md-5">
             <label style="font-size: 10pt">Tanggal Keluar Terakhir</label>
             <input type="text" id="last" class="form-control" readonly>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-5">
             <label style="font-size: 10pt">Biji Sisa</label>
             <input type="number" id="biji_sisa" class="form-control" readonly>
         </div>
