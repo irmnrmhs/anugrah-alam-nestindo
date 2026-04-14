9c099c29c751ec4b0d964bc069524b48fcf697f8
irmnrmhs <irmnrmhs@gmail.com>
2026-04-14 15:49:39 +0700

    fix role seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Role;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class RoleSeeder extends Seeder
 {
@@ -14,15 +13,11 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         Role::create(['name' => 'Super Admin']); //1
+        // Role::create(['name' => 'Admin Rumah Walet']);
+        // Role::create(['name' => 'Admin Bahan Baku']);
         Role::create(['name' => 'Admin Keamanan Pangan']); //2
-        Role::create(['name' => 'Admin Kontrol Kualitas (Data)']); //3
-        Role::create(['name' => 'Admin Kontrol Kualitas (Lab)']); //4
+        Role::create(['name' => 'Admin Produksi']); //3
+        Role::create(['name' => 'Admin Kontrol Kualitas']); //4
         Role::create(['name' => 'Admin Ekspor']); //5
-        Role::create(['name' => 'Admin Human Resource']); //6
-        Role::create(['name' => 'Administrasi']); //7
-        Role::create(['name' => 'Admin Bahan Baku']); //8
-        Role::create(['name' => 'Admin Produksi']); //9
-        Role::create(['name' => 'Admin Akuntansi']); //10
-        Role::create(['name' => 'Admin General Affairs']); //11
     }
 }
